2fdd19356e5a8dbd4b50b30297abf3f529e02ce3
Julien Rosset <julien.rosset@solulog.fr>
2019-07-17 22:03:05 +0200

    Update presentation table

--- epub.bat
+++ epub.bat
@@ -1,2 +1,5 @@
-rm -v Kumo-Desu-Ga-Nani-Ka.epub
-7z a -tzip -mx=9 -mmt=on Kumo-Desu-Ga-Nani-Ka.epub META-INF/ content.opf images/ mimetype style/ text/ toc.ncx
+echo @off
+
+del Kumo-Desu-Ga-Nani-Ka.epub
+"C:\Program Files (x86)\7-Zip\7z.exe" a -tzip -mx=9 -mmt=on Kumo-Desu-Ga-Nani-Ka.epub META-INF/ content.opf images/ mimetype style/ text/ toc.ncx
+
